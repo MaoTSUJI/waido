f73434ec7c56c54636b5f3d620dd9352094cacd6
辻真緒 <>
2019-05-31 11:41:03 +0800

    home.phpカルーセルに画像追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,13 +11,13 @@ Home Page
 		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 		  <div class="carousel-inner">
 		    <div class="carousel-item active">
-		      <img src="..." class="d-block w-100" alt="...">
+		      <img src="{{ asset('/img/background/beach1.jpg') }}" class="d-block w-100" alt="...">
 		    </div>
 		    <div class="carousel-item">
-		      <img src="..." class="d-block w-100" alt="...">
+		      <img src="{{ asset('/img/background/beach2.jpg') }}" class="d-block w-100" alt="...">
 		    </div>
 		    <div class="carousel-item">
-		      <img src="..." class="d-block w-100" alt="...">
+		      <img src="{{ asset('/img/background/beach3.jpg') }}" class="d-block w-100" alt="...">
 		    </div>
 		  </div>
 
@@ -35,7 +35,13 @@ Home Page
 
 	{{-- サイト説明 --}}
 	<div class="container">
-		<h3>ここにサイト説明</h3>
+		<h3>みゃーくふつを救え　「わいどー」</h3>
+		<h3>わいどーとは？</h3>
+		<p>	わいどーとは、宮古島方言「みゃーくふつ」の言葉で「がんばろう」という意味です。
+		ユネスコに消滅危機言語として指定されたみゃーくふつ
+		（参照："<a href="http://www.unesco.org/culture/languages-atlas/en/atlasmap/language-id-1973.html">Atlas of the World’s Languages in Danger</a>"）
+
+		</p>
 	</div>
 
 @endsection
