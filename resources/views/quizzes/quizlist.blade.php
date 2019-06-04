3931093efd44799a3b8994257f066b5a977046e3
motoki <higamotoki5731@gmail.com>
2019-06-04 11:00:22 +0800

    地域選択 スタート画面の挿入

--- resources/views/quizzes/quizlist.blade.php
+++ resources/views/quizzes/quizlist.blade.php
@@ -6,15 +6,20 @@ QuizList
 
 @section('content')
 {{-- ここに中身をかく --}}
+<div id="main_box" class="quiz">
+		<img class="ribbon" src="img/area_select.png" alt="地域選択" width="500px">
+	<div class="text">
 
-{{-- 地域名の表示 --}}
 @foreach($areas as $area)
-	<a href="{{ route('quiz.quizlist_start') }}?name={{ $area->english }} ">
-		<p>{{ $area->id }}</p>
-		<p>{{ $area->hiragana }}</p>
-		<h3>{{ $area->area }}</h3>
-　	</a>
-
+				<a href="{{ route('quiz.quizlist_start') }}?name={{ $area->english }} " class="">
+					<h3>{{ $area->area }}</h3>
+				</a>
 @endforeach
+	</div>
+</div>
+
+
+
+
 
 @endsection
