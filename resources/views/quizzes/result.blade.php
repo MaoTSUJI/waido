533b35d725af663f5651cf3d923364f883e376e3
辻真緒 <>
2019-06-08 11:16:21 +0800

    10問目解答画面「次の問題」→「結果発表」　に修正

--- resources/views/quizzes/result.blade.php
+++ resources/views/quizzes/result.blade.php
@@ -19,13 +19,25 @@ QuizList
 			<!-- 正解数に応じて妖怪を自動変更させる -->
 			<br>
 			<br>
-		<a href="#" class="cp_btn">もう一度とく</a>
+		{{-- クイズスタート画面に遷移 --}}
+		<div>
+			<form action="{{ route('quiz.quiz_area') }}" method="post" class="">
+					@csrf
+					<button class="cp_btn">もう一度とく</button>
+					<input type="hidden" name="area_jpn" value="{{ $_POST['area_jpn'] }}">
+					<input type="hidden" name="area_id" value="{{ $_POST['area_id'] }}">
+					<input type="hidden" name="area_eng" value="{{ $_POST['area_eng'] }}">
+					<input type="hidden" name="qnum" value="0">
+				</form>
+		</div>
+
+
 		<a href="#" class="cp_btn"><i class="fab fa-twitter"></i> シェア</a>
 			<br>
 			<br>
 		<a class="go_ranking" href="#"><i class="fas fa-trophy"></i> ランキングをみる</a>	
 			<br>
-		<a class="town" href="#"><i class="fas fa-map-marker-alt"></i> 地域選択へ戻る</a>	
+		<a class="town" href="{{ route('quiz.quizlist') }}"><i class="fas fa-map-marker-alt"></i> 地域選択へ戻る</a>	
 			<br>
 			<br>
 		</div>
