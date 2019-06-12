64ba4357ccbcd44dc4f824d7aac075473d85731b
辻真緒 <>
2019-06-12 09:25:04 +0800

    labelタグ修正

--- resources/views/quizzes/quizlist.blade.php
+++ resources/views/quizzes/quizlist.blade.php
@@ -11,7 +11,9 @@ QuizList
 		@foreach($areas as $area)
 						<form action="{{ route('quiz.quizlist_start') }}" method="post" class="btn-flat-dashed-filled">
 							@csrf
-							<input type="submit" name="area_jpn" value="{{ $area->area }}">
+							<label for="{{ $area->area }}" class="cp_btn">
+								<input type="submit" name="area_jpn" id="{{ $area->area }}" value="{{ $area->area }}">
+							</label>
 							<input type="hidden" name="area_id" value="{{ $area->id }}">
 							<input type="hidden" name="area_eng" value="{{ $area->english }}">
 						</form>
