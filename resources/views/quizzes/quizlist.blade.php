48437a6f236d2f28c136c8b2ffa3bb4c0f89db0b
辻真緒 <>
2019-06-13 08:31:19 +0800

    quizlistのリンクを画像内へ移動

--- resources/views/quizzes/quizlist.blade.php
+++ resources/views/quizzes/quizlist.blade.php
@@ -10,26 +10,35 @@ QuizList
 	<div id="main_box" class="area">
 
 		<div>
-			<img class="ribbon" src="img/area_select.png" alt="地域選択" width="500px" >
+			<img class="ribbon" src="img/area_select.png" alt="地域選択" width="500px">
 			<br>
-			@foreach($areas as $area)
-			<form method="post" name="form1" action="{{ route('quiz.quizlist_start') }}" style="display:inline;">
-					@csrf
-				  <input type="hidden" name="area_jpn" value="{{ $area->area }}">
-				  <input type="hidden" name="area_id" value="{{ $area->id }}">
-					<input type="hidden" name="area_eng" value="{{ $area->english }}">
-				  <a class="cp_btn" href="javascript:form1[{{ $area->id -1 }}].submit()">{{ $area->area }}</a>
-			</form>
-			@endforeach
+			
 			<br>	
 
-			<div class="map">
+			<div class="map" style="position: relative;">
 				<img class="miyakomap" src=img/miyakojima_.png alt="マップ" usemap="miyako" >
 				<map name="miyako">
 						<area shape="" coords="" href="" alt="" onmouseover="" onfocus="" onmouseout="" onfocus="" >
 						<area shape="" coords="" href="" alt="" onmouseover="" onfocus="" onmouseout="" onfocus="" >
 						<area shape="" coords="" href="" alt="" onmouseover="" onfocus="" onmouseout="" onfocus="" >
 				</map>
+
+				
+				
+					@foreach($areas as $area)
+						<div style="position: absolute; top: 30px; left: {{ ($area->id-1)*200+100 }}px;">
+							<form method="post" name="form1" action="{{ route('quiz.quizlist_start') }}" style="display:inline;">
+								@csrf
+							  <input type="hidden" name="area_jpn" value="{{ $area->area }}">
+							  <input type="hidden" name="area_id" value="{{ $area->id }}">
+								<input type="hidden" name="area_eng" value="{{ $area->english }}">
+							  <a class="cp_btn" href="javascript:form1[{{ $area->id -1 }}].submit()">{{ $area->area }}</a>
+								</form>
+						</div>
+					@endforeach
+				
+
+
 			</div>
 
 		</div>
