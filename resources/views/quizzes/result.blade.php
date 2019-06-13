0e7c754e3bba278adefb93a0a2ba1653f256be9f
辻真緒 <>
2019-06-13 18:27:22 +0800

    style.cssのcp_btnのmarginを追加

--- resources/views/quizzes/result.blade.php
+++ resources/views/quizzes/result.blade.php
@@ -19,16 +19,15 @@ QuizList
 			!</span>
 			<br>
 			<br>
-			<p>{{ $img_url }}</p>
 		<img src="./img/{{ $img_url }}" alt="妖怪の写真" width="250px">
 			<!-- 正解数に応じて妖怪を自動変更させる -->
 			<br>
 			<br>
 		{{-- クイズスタート画面に遷移 --}}
 		<div>
-			<form action="{{ route('quiz.quizlist_start') }}" method="post" class="">
+			<form action="{{ route('quiz.quizlist_start') }}" method="post" class="" name="form1">
 					@csrf
-					<button class="cp_btn">もう一度とく</button>
+						<a style="font-size:16px;" class="cp_btn" href="javascript:form1.submit()">もう一度とく</a>
 					<input type="hidden" name="area_jpn" value="{{ $_POST['area_jpn'] }}">
 					<input type="hidden" name="area_id" value="{{ $_POST['area_id'] }}">
 					<input type="hidden" name="area_eng" value="{{ $_POST['area_eng'] }}">
