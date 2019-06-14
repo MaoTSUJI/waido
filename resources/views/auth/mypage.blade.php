49efa4abe585652e4f468284f85ede1cfbc06a56
Masaki Kono <>
2019-06-14 08:58:51 +0800

    マイページ情報提示を試みるも未完

--- resources/views/auth/mypage.blade.php
+++ resources/views/auth/mypage.blade.php
@@ -14,14 +14,45 @@ Mypage
                 <div class="card-body">
                    {{--  <form method="POST" action="{{ route('verify') }}"> --}}
                         {{-- @csrf --}}
-                    @foreach($users as $user)
-                    <p>{{ $diary['name']}}</p>
-                    <p>{{ $diary['email']}}</p>
-                    {{-- <p>{{ $diary['created_at']}}</p> --}}
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('ユーザー名') }}</label>
 
-                            <div class="col-md-6">
+{{-- <a href="{{ route('diary.create') }}" class="btn btn-outline-primary">新規投稿</a>
+    @foreach($diaries as $diary)
+   
+        <p>{{ $diary['title']}}</p>
+        <p>{{ $diary['body']}}</p>
+        <p>{{ $diary['created_at']}}</p>
+        
+        @if(Auth::check() && Auth::user()->id == $diary['user_id'] )
+        <a class="btn btn-outline-success" href="{{ route('diary.edit', ['id' => $diary['id']]) }}"><i class="fas fa-edit"></i></a>
+        <form action="{{route('diary.destroy',['id'=>$diary['id']])}}" method="POST" class="d-inline">
+            @csrf
+            @method('delete')
+            <button class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+        </form>
+        @endif
+
+    </div>
+    @endforeach
+ --}}
+
+
+                    <div class="m-4 p-4 border border-primary">
+                    @foreach($userinfo as $user)
+                      <div class="form-group row">
+                          <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('ユーザー名') }}</label>
+                            <p>{{ $user['name']}}</p>
+                      </div>      
+
+
+                       <div class="form-group row">
+                          <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
+                             <p>{{ $user['email']}}</p>
+                      </div>
+
+                    {{-- <p>{{ $diary['created_at']}}</p> --}}
+                    @endforeach
+
+                          {{--   <div class="col-md-6">
                                 <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
@@ -32,8 +63,6 @@ Mypage
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
@@ -46,26 +75,13 @@ Mypage
                             </div>
                         </div>
 
-                       {{--  <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
- --}}
                         <div class="form-group row">
                             <label for="result" class="col-md-4 col-form-label text-md-right">{{ __('成績') }}</label>
 
                             <div class="col-md-6">
-                                <input id="result" type="text" class="form-control" name="result" {{-- required --}}>
-                                {{-- 上記textでいいのか？ --}}
+                                <input id="result" type="text" class="form-control" name="result" {{ required --}}
+
                             </div>
                         </div>
 
@@ -76,7 +92,7 @@ Mypage
                                 </button>
                             </div>
                         </div>
-                    </form>
+                    {{-- </form> --}}
                 </div>
             </div>
         </div>
