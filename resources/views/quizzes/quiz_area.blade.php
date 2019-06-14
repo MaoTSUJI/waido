aea5b29a8b46a095c6f4b952b84e1c10db02f459
辻真緒 <>
2019-06-14 12:44:46 +0800

    ハイビスカスの個数修正

--- resources/views/quizzes/quiz_area.blade.php
+++ resources/views/quizzes/quiz_area.blade.php
@@ -61,10 +61,10 @@ QuizList
 
 			<!-- ハイビスカスを使って問題数を表す -->
 			<div class="hibiscus">
-				@for($j=0; $j<=$qnum; $j++)
+				@for($j=1; $j<=$qnum; $j++)
 					<img class="hibiscus1" src="./img/flower_hibiscus.png" alt="問題数" width="40px">
 				@endfor
-				@for($k=0; $k<=(10-$qnum); $k++)
+				@for($k=1; $k<=(10-$qnum); $k++)
 					<img class="hibiscus2" src="./img/flower_hibiscus.png" alt="問題数" width="40px">
 				@endfor
 			</div>
