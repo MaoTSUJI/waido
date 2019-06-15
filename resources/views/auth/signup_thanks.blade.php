68777e86e546f969da1ad2c24c1027d4890fb1ed
Masaki Kono <>
2019-06-15 12:50:47 +0800

    register画面のパスワード見えなくする＆ログイン後のNavbar並び変更

--- resources/views/auth/signup_thanks.blade.php
+++ resources/views/auth/signup_thanks.blade.php
@@ -31,7 +31,8 @@ Signup_thanks
                                  <div class="form-group row">
                                     <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
                                     <div class="col-md-6">
-                                        <p type="password">{{$_POST['password']}} </p>
+                                        <p>・・・・・・・・・・</p>
+                                        {{-- <p type="password">{{$_POST['password']}} </p> --}}
                                     </div>
                                 </div>
                         <button type="button" class="btn btn-light"><a class="nav-link" href="{{ route('quiz.quizlist') }}">クイズをはじめる<span class="sr-only">(current)</span></a></button>
