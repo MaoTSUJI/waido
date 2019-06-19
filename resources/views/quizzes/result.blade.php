626353f7b3bd48bf64c11f36ca2373c07c24ac00
Suzuki Mariko <>
2019-06-19 10:09:37 +0800

    css更新 resultページの見た目変更

--- resources/views/quizzes/result.blade.php
+++ resources/views/quizzes/result.blade.php
@@ -15,13 +15,13 @@ QuizList
 			<p class="score">正解率<strong>{{ $correct_num * 10 }}</strong>%</p>
 			<br>
 		<span>あなたのみゃーくふつレベルは
+			<br>
 			<strong>{{ $title }}</strong>
-			!!</span>
+			</span>
 			<br>
 			<br>
 		<img src="./img/{{ $img_url }}" alt="妖怪の写真" width="250px">
 			<!-- 正解数に応じて妖怪を自動変更させる -->
-			<br>
 			<br>
 		{{-- クイズスタート画面に遷移 --}}
 		<div>
@@ -32,16 +32,16 @@ QuizList
 					<input type="hidden" name="area_id" value="{{ $_POST['area_id'] }}">
 					<input type="hidden" name="area_eng" value="{{ $_POST['area_eng'] }}">
 					<input type="hidden" name="qnum" value="0">
-				</form>
-		</div>
+			{{-- 	</form> --}}
 
 
 		<a href="http://twitter.com/share?url=https://waido.herokuapp.com/&text=【みゃーくふつを救え　わいどー】%0aあなたのみゃーくふつレベルは{{ $title }}です。%0a&hashtags=みゃーくふつ, 宮古島方言, 絶滅危機言語, みゃーくふつを救え, わいどー" target="_blank" class="cp_btn">
 	{{-- 	<a href="https://twitter.com/intent/tweet?text=テキストテキスト%0a改行もできるし%20スペースも&url=https://www.url.url/&hashtags=タグ,二つ目" rel="nofollow" class="cp_btn"> --}}
 
 			<i class="fab fa-twitter icon"></i>  ツイート</a>
-		<a href="https://www.facebook.com/sharer/sharer.php?u=https://waido.herokuapp.com/" target="_blank" class="cp_btn"><i class="fab fa-facebook-f icon"></i>  シェア</a>
-			<br>
+			</form>
+		</div>
+		{{-- <a href="https://www.facebook.com/sharer/sharer.php?u=https://waido.herokuapp.com/" target="_blank" class="cp_btn"><i class="fab fa-facebook-f icon"></i>  シェア</a> --}}
 			<br>
 		<a class="go_ranking" href="{{ route('ranking') }}"><i class="fas fa-trophy"></i> ランキングをみる</a>
 			<br>
